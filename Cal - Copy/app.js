18964d03f98ad833b34565f4c38bb381d13a831e
Souhaib <80969087+Souhaib-789@users.noreply.github.com>
2021-12-24 14:49:13 +0500

    Add files via upload

---
+++ Cal - Copy/app.js
@@ -0,0 +1,15 @@
+function number(val){
+    var w= document.getElementById("result")
+    w.value+=val }
+
+
+   
+function execute(){
+     let s = document.getElementById("result").value
+     let k = eval(s)
+     document.getElementById("result").value = k }
+
+
+   
+function clr(){
+     document.getElementById("result").value = " "}
